11abd660b29adf27fce67bd6bf3bdd062ed7a44d
David Potter <davidgeorgepotter@protonmail.com>
2025-06-03 16:06:15 +0100

    Add users.models.ts with selectAllUsers query invoked

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,60 +1,56 @@
 const testData = require("../data/test/index"); // can be removed when unnecessary
-const { userSchema} = require("../schema/userSchema")
-const {mongoose} = require("mongoose");
-const {chatSchema} = require("../schema/chatSchema");
-const {eventSchema} = require("../schema/eventSchema")
+const { userSchema } = require("../schema/userSchema");
+const { mongoose } = require("mongoose");
+const { chatSchema } = require("../schema/chatSchema");
+const { eventSchema } = require("../schema/eventSchema");
 const { ticketSchema } = require("../schema/ticketSchema");
-const { run } = require("../connection") // DO NOT REMOVE THIS IMPORT!!!! IT WILL BREAK
+const { run } = require("../connection"); // DO NOT REMOVE THIS IMPORT!!!! IT WILL BREAK
 
-const User = mongoose.model("users", userSchema)
-const Chat = mongoose.model("chats", chatSchema)
-const Event = mongoose.model("events", eventSchema)
-const Ticket = mongoose.model("tickets", ticketSchema)
+const User = mongoose.model("users", userSchema);
+const Chat = mongoose.model("chats", chatSchema);
+const Event = mongoose.model("events", eventSchema);
+const Ticket = mongoose.model("tickets", ticketSchema);
 
-const seed = async ({userData, chatData,  eventData, ticketData, }) => {
-
-    try {
-    
+const seed = async ({ userData, chatData, eventData, ticketData }) => {
+  try {
     await User.deleteMany().then(() => {
-        console.log("Successfully deleted old User Data")
-    })
+      console.log("Successfully deleted old User Data");
+    });
     await User.create(userData).then(() => {
-        console.log("Successfully created User Data")
-    })
-    
+      console.log("Successfully created User Data");
+    });
+
     await Chat.deleteMany().then(function () {
-    console.log("Successfully deleted old Chat Data")
-    })
+      console.log("Successfully deleted old Chat Data");
+    });
 
     await Chat.create(chatData).then(
-    console.log("Successfully created Chat Data")
-    )
+      console.log("Successfully created Chat Data")
+    );
 
     await Event.deleteMany().then(function () {
-    console.log("Successfully deleted old Event Data")
-    })
+      console.log("Successfully deleted old Event Data");
+    });
 
     await Event.create(eventData).then(
-        console.log("Successfully created Event Data")
-    )
+      console.log("Successfully created Event Data")
+    );
 
     await Ticket.deleteMany().then(function () {
-        console.log("Successfully deleted old Ticket Data")
-    })
+      console.log("Successfully deleted old Ticket Data");
+    });
 
     await Ticket.create(ticketData).then(
-        console.log("Successfully created Ticket Data")
-    )
-
-    } catch (error) {
-    console.log(error)
-    }   
-    
+      console.log("Successfully created Ticket Data")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // This can be removed when unnecessary
-seed(testData).then(()=>{
-    mongoose.disconnect()
-})
+seed(testData).then(() => {
+  mongoose.disconnect();
+});
 
 module.exports = seed;
