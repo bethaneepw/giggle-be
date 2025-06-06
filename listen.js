f56600cb51014dc1db230f7f497b2fd91aa1a12a
David Potter <davidgeorgepotter@protonmail.com>
2025-06-06 15:51:13 +0100

    Change listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,10 @@
-const app = require("./dist/app.js");
-const { PORT = 9090 } = process.env;
+// const app = require("./dist/app.js");
+// const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+// app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+const app = require("./dist/app.js");
+
+app.listen(1313, () => {
+  console.log("Server is listening on port 1313");
+});
