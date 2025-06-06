5b50f6b01ea86fccc2acbd63aee80c39df2c8bcc
David Potter <davidgeorgepotter@protonmail.com>
2025-06-06 16:01:00 +0100

    Revert listen.js

--- listen.js
+++ listen.js
@@ -1,10 +1,10 @@
-// const app = require("./dist/app.js");
-// const { PORT = 9090 } = process.env;
-
-// app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-
 const app = require("./dist/app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(1313, () => {
-  console.log("Server is listening on port 1313");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+// const app = require("./dist/app.js");
+
+// app.listen(1313, () => {
+//   console.log("Server is listening on port 1313");
+// });
