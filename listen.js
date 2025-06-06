f50d4d006da9de0f129192deb304ee77f3abc3c1
David Potter <davidgeorgepotter@protonmail.com>
2025-06-06 14:29:15 +0100

    Update listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require("./dist/app.js");
-const { PORT } = process.env.PORT || 9090;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
