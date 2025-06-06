381bdc0d8e5cb5cfaf6d4fac1253842ec833ce9b
Andipascale7 <andipascale7@outlook.com>
2025-06-06 16:40:50 +0100

    socketio endpoints02

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { getApi } = require("./controllers/controllers");
-const { getTickets, getTicketById, postTicket, deleteTicket, patchTicket, } = require("./controllers/tickets.controllers");
-const { getEvents, getEventById, postEvent, deleteEvent, } = require("./controllers/events.controllers");
-const { getUsers, postUser, deleteUser, getUserById, patchUser, } = require("./controllers/users.controllers");
 app.use(cors());
-const { handleCustomErrors, catchAllErrors, handleMongoErrors, } = require("./controllers/error.controller");
-// const { getChatbyId, } = require("./controllers/chats.controllers")
-const { postMessagebyId, getMessagebyRoomId, patchMessagebyId} = require("./controllers/messages.controlers")
 app.use(express.json());
+const { getApi } = require("./controllers/controllers");
+const {
+  getTickets,
+  getTicketById,
+  postTicket,
+  deleteTicket,
+  patchTicket,
+} = require("./controllers/tickets.controllers");
+const {
+  getEvents,
+  getEventById,
+  postEvent,
+  deleteEvent,
+} = require("./controllers/events.controllers");
+const {
+  getUsers,
+  postUser,
+  deleteUser,
+  getUserById,
+  patchUser,
+} = require("./controllers/users.controllers");
+const {
+  handleCustomErrors,
+  catchAllErrors,
+  handleMongoErrors,
+} = require("./controllers/error.controller");
+const {
+  postMessagebyId,
+  getMessagebyRoomId,
+  patchMessagebyId,
+} = require("./controllers/messages.controlers");
 app.get("/api", getApi);
 app.get("/api/events", getEvents);
 //queries to add still: date
@@ -28,7 +52,6 @@ app.get("/api/tickets/:ticket_id", getTicketById);
 app.post("/api/tickets", postTicket);
 app.delete("/api/tickets/:ticket_id", deleteTicket);
 app.patch("/api/tickets/:ticket_id", patchTicket);
-// app.get("/api/chats/:chats_id", getChatbyId);
 app.post("/api/messages/:roomId", postMessagebyId);
 app.get("/api/messages/:roomId", getMessagebyRoomId);
 app.delete("/api/messages/:message_id", deleteMessagebyId);
@@ -45,7 +68,7 @@ patch/ tickets
 */
 // Error handling
 app.all("/*splat", (req, res) => {
-    res.status(404).send({ msg: "Invalid url!" });
+  res.status(404).send({ msg: "Invalid url!" });
 });
 app.use(handleMongoErrors);
 app.use(handleCustomErrors);
