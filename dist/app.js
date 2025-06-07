4305ca75d964314b48c449b8a9342c486f0d7123
David Potter <davidgeorgepotter@protonmail.com>
2025-06-07 11:37:20 +0100

    Comment out app.js message endpoint issues that are stopping deploy

--- dist/app.js
+++ dist/app.js
@@ -10,11 +10,6 @@ const { getTickets, getTicketById, postTicket, deleteTicket, patchTicket, } = re
 const { getEvents, getEventById, postEvent, deleteEvent, } = require("./controllers/events.controllers");
 const { getUsers, postUser, deleteUser, getUserById, patchUser, } = require("./controllers/users.controllers");
 const { handleCustomErrors, catchAllErrors, handleMongoErrors, } = require("./controllers/error.controller");
-const {
-    postMessagebyId,
-    getMessagebyRoomId,
-    patchMessagebyId,
-  } = require("./controllers/messages.controlers");
 app.get("/api", getApi);
 app.get("/api/events", getEvents);
 //queries to add still: date
@@ -31,10 +26,6 @@ app.get("/api/tickets/:ticket_id", getTicketById);
 app.post("/api/tickets", postTicket);
 app.delete("/api/tickets/:ticket_id", deleteTicket);
 app.patch("/api/tickets/:ticket_id", patchTicket);
-app.post("/api/messages/:roomId", postMessagebyId);
-app.get("/api/messages/:roomId", getMessagebyRoomId);
-app.delete("/api/messages/:message_id", deleteMessagebyId);
-app.patch("/api/messages/:message_id", patchMessagebyId);
 /*
 
 To-do:
